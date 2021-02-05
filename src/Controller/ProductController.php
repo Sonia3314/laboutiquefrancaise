2b2553f9b50613a88106f41503d004b490b97cf9
Sonia3314 <Sonia3314@users.noreply.github.com>
2021-02-05 16:04:51 +0100

    Search creation

--- src/Controller/ProductController.php
+++ src/Controller/ProductController.php
@@ -3,7 +3,9 @@
 namespace App\Controller;
 
 
+use App\Classe\Search;
 use App\Entity\Product;
+use App\Form\SearchType;
 use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -25,9 +27,14 @@ class ProductController extends AbstractController
     public function index(): Response
     {   
         $products = $this->entityManager->getRepository(Product::class)->findAll();
+        
+        $search = new Search();
+        
+        $form = $this->createForm(SearchType::class, $search);
 
         return $this->render('product/index.html.twig', [
-            'products' => $products
+            'products' => $products,
+            'form' => $form->createView()
         ]);
     }
 
